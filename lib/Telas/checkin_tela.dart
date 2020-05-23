dafa38b373ae9990c6fc5ff3397f51c5471aa549
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-05-23 15:04:47 -0300

    feat(checkin_tela): cria esboço tela de confirmação de corte

--- lib/Telas/checkin_tela.dart
+++ lib/Telas/checkin_tela.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:slider_button/slider_button.dart';
 
 class CheckinTela extends StatefulWidget {
   @override
@@ -8,6 +9,42 @@ class CheckinTela extends StatefulWidget {
 class _CheckinTelaState extends State<CheckinTela> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Checkin do cliente nomeLegal"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Text("O valor foi x e será pago via bufunfa virtual :D"),
+          SizedBox(
+            height: 44,
+            width: 250,
+            child: SliderButton(
+              dismissible: false,
+              shimmer: false,
+              action: () {
+
+                Navigator.of(context).pop();
+              },
+              label: Text(
+                "Confirmar",
+                style: TextStyle(
+                    color: Color(0xff4a4a4a),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 17),
+              ),
+              icon: Container(
+                width: 0,
+                height: 0,
+              ),
+              highlightedColor: Colors.white70,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
