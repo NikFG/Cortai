84f5b163a1913eb36143d1d346550f940b2b686f
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-06-09 17:37:46 -0300

    feat(home_tela): adapta home_tela para o BottomNavigationBar

--- lib/Telas/home_tela.dart
+++ lib/Telas/home_tela.dart
@@ -1,6 +1,7 @@
 import 'package:agendacabelo/Tiles/home_tab.dart';
 import 'package:agendacabelo/Telas/gerenciar_salao_tela.dart';
 import 'package:agendacabelo/Util/push_notification.dart';
+import 'package:agendacabelo/Widgets/bottom_custom.dart';
 import 'package:agendacabelo/Widgets/drawer_custom.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -8,20 +9,34 @@ import 'marcado_tela.dart';
 import 'criar_servico_tela.dart';
 import 'confirmar_tela.dart';
 
-class HomeTela extends StatelessWidget {
+class HomeTela extends StatefulWidget {
   final String usuario_id;
   final int paginaInicial;
 
   HomeTela({this.usuario_id, this.paginaInicial});
 
   @override
+  _HomeTelaState createState() => _HomeTelaState();
+}
+
+class _HomeTelaState extends State<HomeTela> {
+  int index = 0;
+
+  @override
   Widget build(BuildContext context) {
-    final _pageController =
-        PageController(initialPage: paginaInicial == null ? 0 : paginaInicial);
-    if (usuario_id != null) PushNotification.servico(usuario_id, context);
+    final _pageController = PageController(
+        initialPage:
+            widget.paginaInicial == null ? index : widget.paginaInicial);
+    if (widget.usuario_id != null)
+      PushNotification.servico(widget.usuario_id, context);
     return PageView(
       physics: NeverScrollableScrollPhysics(),
       controller: _pageController,
+      onPageChanged: (i) {
+        setState(() {
+          index = i;
+        });
+      },
       children: <Widget>[
         Scaffold(
           appBar: AppBar(
@@ -30,10 +45,11 @@ class HomeTela extends StatelessWidget {
             centerTitle: true,
           ),
           body: HomeTab(),
-          drawer: DrawerCustom(_pageController),
+//          bottomNavigationBar: BottomCustom(_pageController, index),
+          bottomNavigationBar: BottomCustom(_pageController, index),
         ),
         Scaffold(
-          drawer: DrawerCustom(_pageController),
+          bottomNavigationBar: BottomCustom(_pageController, index),
           body: MarcadoTela(),
           appBar: AppBar(
             backgroundColor: Theme.of(context).primaryColor,
@@ -42,7 +58,7 @@ class HomeTela extends StatelessWidget {
           ),
         ),
         Scaffold(
-          drawer: DrawerCustom(_pageController),
+          bottomNavigationBar: BottomCustom(_pageController, index),
           body: ConfirmarTela(),
           appBar: AppBar(
             backgroundColor: Theme.of(context).primaryColor,
@@ -84,7 +100,7 @@ class HomeTela extends StatelessWidget {
           ),
         ),
         Scaffold(
-          drawer: DrawerCustom(_pageController),
+          bottomNavigationBar: BottomCustom(_pageController, index),
           body: CriarServicoTela(),
           appBar: AppBar(
             backgroundColor: Theme.of(context).primaryColor,
@@ -93,7 +109,7 @@ class HomeTela extends StatelessWidget {
           ),
         ),
         Scaffold(
-          drawer: DrawerCustom(_pageController),
+          bottomNavigationBar: BottomCustom(_pageController, index),
           body: GerenciarSalaoTela(),
           appBar: AppBar(
             backgroundColor: Theme.of(context).primaryColor,
