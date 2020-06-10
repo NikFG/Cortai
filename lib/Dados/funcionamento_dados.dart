90a20ad193d9ff6b21d7c1ab20c22324eeb6b79a
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-06-10 19:23:47 -0300

    feat: cria métodos store e update para as classes de dados

--- lib/Dados/funcionamento_dados.dart
+++ lib/Dados/funcionamento_dados.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class FuncionamentoDados {
   String diaSemana;
@@ -21,5 +22,37 @@ class FuncionamentoDados {
       "horarioFechamento": horarioFechamento,
       "intervalo": intervalo,
     };
+  }
+
+  store(FuncionamentoDados dados, String salao,
+      {@required VoidCallback onSuccess, @required VoidCallback onFail}) async {
+    await Firestore.instance
+        .collection('saloes')
+        .document(salao)
+        .collection('funcionamento')
+        .add(dados.toMap())
+        .then((value) {
+      print(value);
+      onSuccess();
+    }).catchError((e) {
+      print(e);
+      onFail();
+    });
+  }
+
+  update(FuncionamentoDados dados, String salao,
+      {@required VoidCallback onSuccess, @required VoidCallback onFail}) async {
+    await Firestore.instance
+        .collection('saloes')
+        .document(salao)
+        .collection('funcionamento')
+        .document(dados.diaSemana)
+        .updateData(dados.toMap())
+        .then((value) {
+      onSuccess();
+    }).catchError((e) {
+      print(e);
+      onFail();
+    });
   }
 }
