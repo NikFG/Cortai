4a7d08b42e2bbfe27c6680e0a50f3cd0ec21e140
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-06-10 17:39:06 -0300

    feat: cria permissões de usuário na bottom navigation

--- lib/Widgets/bottom_custom.dart
+++ lib/Widgets/bottom_custom.dart
@@ -1,48 +1,42 @@
+import 'package:badges/badges.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
 class BottomCustom extends StatefulWidget {
   final PageController pageController;
   final int index;
+  final bool isCabeleireiro;
+  final String usuario;
 
-  BottomCustom(this.pageController, this.index);
+  BottomCustom(
+      this.pageController, this.index, this.isCabeleireiro, this.usuario);
 
   @override
   _BottomCustomState createState() => _BottomCustomState();
 }
 
 class _BottomCustomState extends State<BottomCustom> {
+  List<BottomNavigationBarItem> itens = [];
+
+  @override
+  void initState() {
+    super.initState();
+    itensUsuario();
+    if (widget.isCabeleireiro) {
+      itensCabeleireiro(widget.usuario);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      currentIndex: widget.index,
-      onTap: (index) {
-        _bottomTapped(index);
-      },
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text("Início"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.schedule),
-          title: Text("Marcados"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(FontAwesome5.calendar_check),
-          title: Text("Confirmar"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.content_cut),
-          title: Text("Serviços"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.store),
-          title: Text("Salão"),
-        ),
-      ],
-    );
+        type: BottomNavigationBarType.fixed,
+        currentIndex: widget.index,
+        onTap: (index) {
+          _bottomTapped(index);
+        },
+        items: itens);
   }
 
   void _bottomTapped(int index) {
@@ -50,5 +44,63 @@ class _BottomCustomState extends State<BottomCustom> {
       widget.pageController.animateToPage(index,
           duration: Duration(milliseconds: 500), curve: Curves.ease);
     });
+  }
+
+  itensUsuario() {
+    var itens = [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        title: Text("Início"),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.schedule),
+        title: Text("Marcados"),
+      ),
+    ];
+    this.itens.addAll(itens);
+  }
+
+  itensCabeleireiro(String uid) {
+    var itens = [
+      BottomNavigationBarItem(
+        icon: StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance
+                .collection("horarios")
+                .where('confirmado', isEqualTo: false)
+                .where('ocupado', isEqualTo: true)
+                .where('cabeleireiro', isEqualTo: uid)
+                .snapshots(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Icon(FontAwesome5.calendar_check);
+                default:
+                  int _numeroConfirmacoes = snapshot.data.documents.length;
+                  return Badge(
+                    badgeColor: Theme.of(context).primaryColor,
+                    showBadge: _numeroConfirmacoes != 0 ? true : false,
+                    animationType: BadgeAnimationType.scale,
+                    position: BadgePosition(left: 19, bottom: 8),
+                    badgeContent: Text(
+                      _numeroConfirmacoes.toString(),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    child: Icon(FontAwesome5.calendar_check),
+                  );
+              }
+            }),
+        title: Text("Confirmar"),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.content_cut),
+        title: Text("Serviços"),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.store),
+        title: Text("Salão"),
+      ),
+    ];
+    this.itens.addAll(itens);
   }
 }
