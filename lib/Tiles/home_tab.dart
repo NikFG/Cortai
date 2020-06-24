34363945fed3b0032ba470679c053ece4f205dfe
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-06-24 15:18:09 -0300

    feat(home_tab): atualiza retorno de api
    Erro 404 adicionado ao código

--- lib/Tiles/home_tab.dart
+++ lib/Tiles/home_tab.dart
@@ -1,12 +1,9 @@
 import 'dart:convert';
-
-import 'package:agendacabelo/Controle/salao_controle.dart';
+import 'package:agendacabelo/Controle/shared_preferences_controle.dart';
 import 'package:agendacabelo/Dados/salao_dados.dart';
 import 'package:agendacabelo/Tiles/home_tile.dart';
-import 'package:agendacabelo/Util/haversine.dart';
 import 'package:agendacabelo/Widgets/carousel.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:http/http.dart' as http;
@@ -17,13 +14,7 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-  int cont = 0;
   PermissionStatus _permissionStatus = PermissionStatus.undetermined;
-  int limite = 4;
-  bool temMais = true;
-  bool isLoading = false;
-  DocumentSnapshot ultimoSalao;
-  List<DocumentSnapshot> saloes = [];
   ScrollController _scrollController = ScrollController();
   var local = [];
   var endereco = TextEditingController();
@@ -36,12 +27,6 @@ class _HomeTabState extends State<HomeTab> {
     if (_permissionStatus.isUndetermined)
       requestPermission(Permission.location);
     getEndereco();
-//      double currentScroll = _scrollController.position.pixels;
-//      double delta = MediaQuery.of(context).size.height * 0.25;
-//      if (maxScroll - currentScroll <= delta) {
-//        getSaloes();
-//      }
-//    });
   }
 
   @override
@@ -103,10 +88,6 @@ class _HomeTabState extends State<HomeTab> {
                       child: CircularProgressIndicator(),
                     );
                   } else {
-                //TODO Se permissão negada perguntar endereço de casa
-                if (_permissionStatus.isUndetermined ||
-                    _permissionStatus.isDenied)
-                  requestPermission(Permission.location);
                     var currentLocation = localizacao.data;
 
                     if (local.isNotEmpty)
@@ -127,53 +108,37 @@ class _HomeTabState extends State<HomeTab> {
                               placemark.data.first.subAdministrativeArea;
                           var url =
                               "$_link?cidade=$cidade&lat=${lat.toString()}&lng=${lng.toString()}";
-                      return FutureBuilder<http.Response>(
-                        future: http.get(url),
-                        builder: (context, response) {
-                          if (!response.hasData) {
-                            return Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          } else {
-//                            if (response.data.statusCode == 200)
 
-                            List<dynamic> dados =
-                                json.decode(response.data.body);
-                            List<Widget> widgets = dados
-                                .map((s) => HomeTile(SalaoDados.fromJson(s),
-                                    s['distancia'] as double))
-                                .toList();
-
-//
-
-                            return Column(
-                              children: widgets,
-                            );
-                          }
-                        },
-                      );
-                    }
-                  },
-                );
-
-//                List<Widget> widgets = lista
-//                    .map((doc) =>
-//                        HomeTile(SalaoDados.fromDocument(doc), currentLocation))
-//                    .toList();
-
-//                return Column(
-//                  children: widgets,
-//                );
-              }
-            }),
-        isLoading
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : Container(
-                height: 0,
-                width: 0,
-              )
+                          return FutureBuilder<http.Response>(
+                            future: http.get(url),
+                            builder: (context, response) {
+                              if (!response.hasData) {
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              } else {
+                                if (response.data.statusCode == 404) {
+                                  return Center(
+                                    child: Text(response.data.body),
+                                  );
+                                }
+                                List<dynamic> dados =
+                                    json.decode(response.data.body);
+                                List<Widget> widgets = dados
+                                    .map((s) => HomeTile(SalaoDados.fromJson(s),
+                                        s['distancia'] as double))
+                                    .toList();
+                                return Column(
+                                  children: widgets,
+                                );
+                              }
+                            },
+                          );
+                        }
+                      },
+                    );
+                  }
+                }),
       ],
     );
   }
@@ -192,7 +157,6 @@ class _HomeTabState extends State<HomeTab> {
       return true;
     }
     return false;
-      return;
   }
 
   getEndereco() async {
@@ -201,26 +165,7 @@ class _HomeTabState extends State<HomeTab> {
       if (endereco.isNotEmpty) {
         this.endereco.text = endereco;
         local = await Geolocator().placemarkFromAddress(endereco);
-          .orderBy('cidade')
-          .orderBy('nome')
-          .limit(limite)
-          .getDocuments();
-    } else {
-      querySnapshot = await SalaoControle.get()
-          .where('cidade', isEqualTo: 'Divinópolis')
-          .orderBy('cidade')
-          .orderBy('nome')
-          .startAfterDocument(ultimoSalao)
-          .limit(limite)
-          .getDocuments();
       }
-    if (querySnapshot.documents.length < limite) {
-      temMais = false;
     }
-    ultimoSalao = querySnapshot.documents[querySnapshot.documents.length - 1];
-    saloes.addAll(querySnapshot.documents);
-    setState(() {
-      isLoading = false;
-    });
   }
 }
