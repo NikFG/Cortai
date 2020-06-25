0dcd02a1d16e2dd851a812b9e072e15c072d223c
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-06-25 13:44:27 -0300

    feat(home_tile): modifica widget tree para trabalhar em cima de card

--- lib/Tiles/home_tile.dart
+++ lib/Tiles/home_tile.dart
@@ -9,7 +9,10 @@ import 'package:agendacabelo/Telas/servico_tela.dart';
 import 'package:agendacabelo/Widgets/hero_custom.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:getflutter/components/avatar/gf_avatar.dart';
+import 'package:getflutter/shape/gf_avatar_shape.dart';
 
 class HomeTile extends StatefulWidget {
   final SalaoDados dados;
@@ -40,143 +43,71 @@ class _HomeTileState extends State<HomeTile> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
-      child: GestureDetector(
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: InkWell(
         onTap: () => Navigator.of(context).push(MaterialPageRoute(
             builder: (context) => ServicoTela(
                 dados: widget.dados,
                 menorValor: _menorValor,
                 maiorValor: _maiorValor,
                 distancia: _distancia))),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey[300], width: 1.0),
+        child: Card(
+          shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.grey[300], width: 1.0),
               borderRadius: BorderRadius.all(Radius.circular(10))),
-          child: Row(
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) =>
-                          HeroCustom(imagemUrl: widget.dados.imagem)));
-                },
-                child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Colors.transparent,
-                      backgroundImage: NetworkImage(widget.dados.imagem),
-                    )),
+          child: ListTile(
+            leading: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        HeroCustom(imagemUrl: widget.dados.imagem)));
+              },
+              child: GFAvatar(
+                shape: GFAvatarShape.circle,
+                radius: 30,
+                backgroundColor: Colors.transparent,
+                backgroundImage: NetworkImage(widget.dados.imagem),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(10.0),
-                    width: MediaQuery.of(context).size.width - 180,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          widget.dados.nome,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20.0,
-                              fontFamily: 'Poppins'),
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  _media.toStringAsFixed(1),
-                                  style: TextStyle(
-                                      fontSize: 14.0, color: Colors.black38),
-                                ),
-                                SizedBox(
-                                  width: 2.0,
-                                ),
-                                Icon(Icons.star,
-                                    color: Colors.amber, size: 10.0),
-                                SizedBox(
-                                  width: 5.0,
-                                ),
-                                Text(
-                                  "(${_quantidade.toString()})",
-                                  style: TextStyle(
-                                      fontSize: 14.0, color: Colors.black38),
-                                ),
-                                SizedBox(
-                                  width: 5.0,
-                                ),
-                                Text(
-                                  _distancia,
-                                  style: TextStyle(
-                                      fontSize: 14.0, color: Colors.black38),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: Text(
-                              "Entre R\$${_menorValor.toStringAsFixed(2)} ~ R\$${_maiorValor.toStringAsFixed(2)} ",
-                              style: TextStyle(
-                                  fontSize: 11, color: Colors.black38),
-                            ))
-                      ],
+            ),
+            title: Text(
+              widget.dados.nome,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.left,
+              softWrap: false,
+              style: TextStyle(
+                  color: Colors.black, fontSize: 20.0, fontFamily: 'Poppins'),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      _media.toStringAsFixed(1),
+                      style: TextStyle(fontSize: 14.0, color: Colors.black38),
                     ),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      IconButton(
-                        padding: EdgeInsets.only(top: 10),
-                        alignment: Alignment.bottomCenter,
-                        icon: Icon(
-                          Icons.arrow_forward_ios,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        onPressed: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) => ServicoTela(
-                                    dados: widget.dados,
-                                    menorValor: _menorValor,
-                                    maiorValor: _maiorValor,
-                                    distancia: _distancia))),
-                      ),
-                      ButtonTheme(
-                          padding: EdgeInsets.zero,
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                          minWidth: 0,
-                          height: 0,
-                          child: FlatButton(
-                            materialTapTargetSize: MaterialTapTargetSize.padded,
-                            visualDensity: VisualDensity.compact,
-                            padding: EdgeInsets.zero,
-                            onPressed: () => _dialogDados(context),
-                            child: Text(
-                              'Saiba mais',
-                              overflow: TextOverflow.visible,
-                              style: TextStyle(
-                                  fontSize: 10,
-                                  color: Theme.of(context).primaryColor),
-                            ),
-                          )),
-                    ],
-                  )
-                ],
-              )
-            ],
+                    SizedBox(width: 2.0),
+                    Icon(Icons.star, color: Colors.amber, size: 10.0),
+                    SizedBox(width: 5.0),
+                    Text(
+                      "(${_quantidade.toString()})",
+                      style: TextStyle(fontSize: 14.0, color: Colors.black38),
+                    ),
+                    SizedBox(width: 5.0),
+                    Text(
+                      _distancia,
+                      style: TextStyle(fontSize: 14.0, color: Colors.black38),
+                    ),
+                  ],
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: Text(
+                      "Entre R\$${_menorValor.toStringAsFixed(2)} ~ R\$${_maiorValor.toStringAsFixed(2)} ",
+                      style: TextStyle(fontSize: 11, color: Colors.black38),
+                    )),
+              ],
+            ),
           ),
         ),
       ),
