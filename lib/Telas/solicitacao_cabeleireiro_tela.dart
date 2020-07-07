d11fc66d03e94068dc87c639a5bd2df31b26eae6
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-07-06 21:18:08 -0300

    feat: cria validações nos botoes e no formfield

--- lib/Telas/solicitacao_cabeleireiro_tela.dart
+++ lib/Telas/solicitacao_cabeleireiro_tela.dart
@@ -1,7 +1,7 @@
 import 'package:agendacabelo/Dados/login_dados.dart';
+import 'package:agendacabelo/Widgets/custom_form_field.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:email_validator/email_validator.dart';
-import 'package:flushbar/flushbar.dart';
 import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -19,12 +19,11 @@ class _SolicitacaoCabeleireiroTelaState
     extends State<SolicitacaoCabeleireiroTela> {
   final _emailControlador = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool _botaoHabilitado = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
       appBar: (AppBar(
         title: Text("Cadastrar cabeleireiro"),
         centerTitle: true,
@@ -34,11 +33,11 @@ class _SolicitacaoCabeleireiroTelaState
         child: ListView(
           padding: EdgeInsets.all(10),
           children: <Widget>[
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
+            CustomFormField(
               controller: _emailControlador,
-              decoration: InputDecoration(
-                  hintText: 'Email', hintStyle: TextStyle(fontSize: 12)),
+              icon: Icon(Icons.mail),
+              hint: "Email",
+              inputType: TextInputType.emailAddress,
               validator: (text) {
                 if (text.isEmpty || !EmailValidator.validate(text)) {
                   return "Email inválido";
@@ -46,29 +45,52 @@ class _SolicitacaoCabeleireiroTelaState
                 return null;
               },
             ),
-            RaisedButton(
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  await Firestore.instance
-                      .collection('usuarios')
-                      .where('email', isEqualTo: _emailControlador.text)
-                      .getDocuments()
-                      .then((value) async {
-                    LoginDados dados =
-                        LoginDados.fromDocument(value.documents[0].data);
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 45,
+              child: RaisedButton(
+                color: Color(0xFFf45d27),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                onPressed: _botaoHabilitado
+                    ? () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() {
+                            _botaoHabilitado = false;
+                          });
+                          await Firestore.instance
+                              .collection('usuarios')
+                              .where('email', isEqualTo: _emailControlador.text)
+                              .getDocuments()
+                              .then((value) async {
+                            LoginDados dados = LoginDados.fromDocument(
+                                value.documents[0].data);
 
-                    await Firestore.instance
-                        .collection('usuarios')
-                        .document(dados.id)
-                        .updateData(({'salaoTemp': widget.salao}));
-                    onSuccess();
-                  }).catchError((e) {
-                    print(e);
-                    onFail();
-                  });
-                }
-              },
-              child: Text("Confirmar"),
+                            await Firestore.instance
+                                .collection('usuarios')
+                                .document(dados.id)
+                                .updateData(({'salaoTemp': widget.salao}));
+                            onSuccess();
+                          }).catchError((e) {
+                            print(e);
+                            onFail();
+                          });
+                        }
+                      }
+                    : null,
+                child: _botaoHabilitado
+                    ? Text(
+                        'Confirmar',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    : CircularProgressIndicator(),
+              ),
             )
           ],
         ),
@@ -82,7 +104,12 @@ class _SolicitacaoCabeleireiroTelaState
   }
 
   onFail() async {
-    await FlushbarHelper.createError(message: "Houve um erro ao enviar o email")
+    await FlushbarHelper.createError(
+            title: "Verifique o email digitado",
+            message: "Houve um erro ao enviar o email")
         .show(context);
+    setState(() {
+      _botaoHabilitado = true;
+    });
   }
 }
