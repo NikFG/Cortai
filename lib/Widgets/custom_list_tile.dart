a858ed4bc5f2249717ebe3c3edbe5ca9a01dc135
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-07-13 12:59:33 -0300

    feat(custom_list_tile): cria list tile padrão

---
+++ lib/Widgets/custom_list_tile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomListTile extends StatelessWidget {
+  final GestureTapCallback onTap;
+  final Widget leading;
+  final Widget title;
+  final Widget subtitle;
+  final Widget trailing;
+
+  CustomListTile(
+      {@required this.leading,
+      @required this.title,
+      @required this.onTap,
+      this.subtitle,
+      this.trailing});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 5),
+      height: 120,
+      child: Card(
+          shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.grey[300], width: 1.0),
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: Align(
+            alignment: Alignment.center,
+            child: ListTile(
+              onTap: onTap,
+              leading: leading,
+              title: title,
+              subtitle: subtitle,
+              trailing: trailing,
+            ),
+          )),
+    );
+  }
+}
