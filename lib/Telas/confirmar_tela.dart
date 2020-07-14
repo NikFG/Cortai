81f0056c9280c12d2fa0b6f75fbe71d8d22ce931
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2020-07-14 10:44:15 -0300

    feat(confirmar_tela): cria view diferenciando a confirmar e confirmados

--- lib/Telas/confirmar_tela.dart
+++ lib/Telas/confirmar_tela.dart
@@ -6,46 +6,82 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class ConfirmarTela extends StatelessWidget {
-  ConfirmarTela();
+class ConfirmarTela extends StatefulWidget {
+  @override
+  _ConfirmarTelaState createState() => _ConfirmarTelaState();
+}
 
+class _ConfirmarTelaState extends State<ConfirmarTela> {
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<LoginModelo>(
       builder: (context, child, model) {
         if (model.dados != null)
-          return Material(
-            child: StreamBuilder<QuerySnapshot>(
-                stream: HorarioControle.get()
-                    .where('confirmado', isEqualTo: false)
-                    .where('cabeleireiro', isEqualTo: model.dados.id)
-                    .orderBy('data')
-                    .orderBy('horario')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else {
-                    if (snapshot.data.documents.length == 0) {
+          return TabBarView(
+            children: <Widget>[
+              StreamBuilder<QuerySnapshot>(
+                  stream: HorarioControle.get()
+                      .where('confirmado', isEqualTo: false)
+                      .where('cabeleireiro', isEqualTo: model.dados.id)
+                      .orderBy('data')
+                      .orderBy('horario')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
                       return Center(
-                        child: Text("Não há agendamentos para confirmar"),
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      if (snapshot.data.documents.length == 0) {
+                        return Center(
+                          child: Text("Não há agendamentos para confirmar"),
+                        );
+                      }
+                      var dividedTiles = ListTile.divideTiles(
+                              tiles: snapshot.data.documents.map((doc) {
+                                return ConfirmarTile(
+                                    HorarioDados.fromDocument(doc), true);
+                              }).toList(),
+                              color: Colors.grey[500],
+                              context: context)
+                          .toList();
+                      return ListView(
+                        children: dividedTiles,
                       );
                     }
-                    var dividedTiles = ListTile.divideTiles(
-                            tiles: snapshot.data.documents.map((doc) {
-                              return ConfirmarTile(
-                                  HorarioDados.fromDocument(doc));
-                            }).toList(),
-                            color: Colors.grey[500],
-                            context: context)
-                        .toList();
-                    return ListView(
-                      children: dividedTiles,
-                    );
-                  }
-                }),
+                  }),
+              FutureBuilder<QuerySnapshot>(
+                  future: HorarioControle.get()
+                      .where('confirmado', isEqualTo: true)
+                      .where('cabeleireiro', isEqualTo: model.dados.id)
+                      .orderBy('data')
+                      .orderBy('horario')
+                      .getDocuments(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      if (snapshot.data.documents.length == 0) {
+                        return Center(
+                          child: Text("Não há agendamentos para confirmar"),
+                        );
+                      }
+                      var dividedTiles = ListTile.divideTiles(
+                              tiles: snapshot.data.documents.map((doc) {
+                                return ConfirmarTile(
+                                    HorarioDados.fromDocument(doc), false);
+                              }).toList(),
+                              color: Colors.grey[500],
+                              context: context)
+                          .toList();
+                      return ListView(
+                        children: dividedTiles,
+                      );
+                    }
+                  }),
+            ],
           );
         else
           return Center();
