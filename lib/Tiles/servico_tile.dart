5a1bcfdf7866eec8623177865e82754e71a51284
Nikollas <nikollasferreirag@gmail.com>
2020-07-16 14:23:34 -0300

    feat(servico_tile): padroniza list tile

--- lib/Tiles/servico_tile.dart
+++ lib/Tiles/servico_tile.dart
@@ -1,11 +1,12 @@
-import 'package:agendacabelo/Dados/servico_dados.dart';
+import 'package:agendacabelo/Dados/servico.dart';
 import 'package:agendacabelo/Telas/agenda_tela.dart';
+import 'package:agendacabelo/Widgets/custom_list_tile.dart';
 import 'package:agendacabelo/Widgets/hero_custom.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ServicoTile extends StatelessWidget {
-  final ServicoDados dados;
+  final Servico dados;
   final String nomeSalao;
   final String imgPadrao =
       "https://images.unsplash.com/photo-1534778356534-d3d45b6df1da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
@@ -14,57 +15,46 @@ class ServicoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 5),
-      height: 100,
-      child: Card(
-          shape: RoundedRectangleBorder(
-              side: BorderSide(color: Colors.grey[300], width: 1.0),
-              borderRadius: BorderRadius.all(Radius.circular(10))),
-          child: Align(
-            alignment: Alignment.center,
-            child: ListTile(
-              onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) =>
-                      AgendaTela(this.dados, this.nomeSalao))),
-              leading: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => HeroCustom(
-                            imagemUrl: dados.imagemUrl != null
-                                ? dados.imagemUrl
-                                : imgPadrao,
-                            descricao: dados.descricao,
-                          )));
-                },
-                child: CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  radius: 30,
-                  backgroundImage: NetworkImage(
-                      dados.imagemUrl != null ? dados.imagemUrl : imgPadrao,
-                      scale: 2.0),
-                ),
-              ),
-              title: Text(
-                dados.descricao,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                    color: Colors.black, fontSize: 20.0, fontFamily: 'Poppins'),
-              ),
-              subtitle: Text(
-                dados.observacao,
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                ),
-                maxLines: 3,
-              ),
-              trailing: Text(
-                "R\$${dados.valor.toStringAsFixed(2).replaceAll('.', ',')}",
-                style: TextStyle(
-                    fontSize: 14, color: Colors.black87, fontFamily: 'Poppins'),
-              ),
-            ),
-          )),
+    return CustomListTile(
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) =>
+              AgendaTela(this.dados, this.nomeSalao))),
+      leading: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => HeroCustom(
+                    imagemUrl: dados.imagemUrl != null
+                        ? dados.imagemUrl
+                        : imgPadrao,
+                    descricao: dados.descricao,
+                  )));
+        },
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 30,
+          backgroundImage: NetworkImage(
+              dados.imagemUrl != null ? dados.imagemUrl : imgPadrao,
+              scale: 2.0),
+        ),
+      ),
+      title: Text(
+        dados.descricao,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+            color: Colors.black, fontSize: 20.0, fontFamily: 'Poppins'),
+      ),
+      subtitle: Text(
+        dados.observacao,
+        style: TextStyle(
+          fontFamily: 'Poppins',
+        ),
+        maxLines: 3,
+      ),
+      trailing: Text(
+        "R\$${dados.valor.toStringAsFixed(2).replaceAll('.', ',')}",
+        style: TextStyle(
+            fontSize: 14, color: Colors.black87, fontFamily: 'Poppins'),
+      ),
     );
   }
 }
