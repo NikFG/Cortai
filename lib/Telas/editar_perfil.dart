2bfdedee07e0c489a58713c669dccd2c707eb460
Nikollas <nikollasferreirag@gmail.com>
2020-07-17 16:35:48 -0300

    feat(editar_perfil): recebe dados do firebase e mudança de senha
    A mudança de senha é dada por meio da recuperação dela via firebase

--- lib/Telas/editar_perfil.dart
+++ lib/Telas/editar_perfil.dart
@@ -1,158 +1,183 @@
 import 'dart:io';
 import 'package:agendacabelo/Controle/salao_controle.dart';
+import 'package:agendacabelo/Dados/login.dart';
 import 'package:agendacabelo/Dados/salao.dart';
+import 'package:agendacabelo/Modelos/login_modelo.dart';
+import 'package:agendacabelo/Telas/home_tela.dart';
 import 'package:agendacabelo/Util/util.dart';
 import 'package:agendacabelo/Widgets/custom_form_field.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flushbar/flushbar.dart';
+import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:password_strength/password_strength.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class EditarPerfilTela extends StatefulWidget {
-  EditarPerfilTela();
+  final Login login;
+
+  EditarPerfilTela(this.login);
 
   @override
   _EditarPerfilTelaState createState() => _EditarPerfilTelaState();
 }
 
 class _EditarPerfilTelaState extends State<EditarPerfilTela> {
+  var _formKey = GlobalKey<FormState>();
   final _nomeControlador = TextEditingController();
   final _telefoneControlador = MaskedTextController(mask: '(00) 0 0000-0000');
-  final _senhaControlador = TextEditingController();
-  final _senhaConfirmaControlador = TextEditingController();
+  bool _botaoHabilitado = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _nomeControlador.text = widget.login.nome;
+    _telefoneControlador.text = widget.login.telefone;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Editar Perfil"),
-        centerTitle: true,
-        leading: Util.leadingScaffold(context),
-      ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text("Nome completo",
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 16,
-                      color: Colors.grey,
-                    )),
-                SizedBox(height: 5),
-                CustomFormField(
-                    hint:
-                        "se tiver como pegar o nome do sujeito no banco fica topster",
-                    icon: Icon(
-                      FontAwesome.user,
-                      color: Colors.grey,
-                    ),
-                    inputType: TextInputType.emailAddress,
-                    controller: _nomeControlador,
-                    validator: (text) {
-                      return null;
-                    }),
-                SizedBox(height: 5),
-                Text("Telefone",
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 16,
-                      color: Colors.grey,
-                    )),
-                SizedBox(height: 5),
-                CustomFormField(
-                    hint: "(99)9 9999-9999",
-                    icon: Icon(
-                      FontAwesome.phone,
-                      color: Colors.grey,
-                    ),
-                    controller: _telefoneControlador,
-                    inputType: TextInputType.phone,
-                    validator: (text) {
-                      if (text.isEmpty || text.length < 11) {
-                        return "Numero invalido";
-                      }
-                      return null;
-                    }),
-                SizedBox(height: 5),
-                Text("Senha",
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 16,
-                      color: Colors.grey,
-                    )),
-                SizedBox(height: 5),
-                CustomFormField(
-                    hint: "**********",
-                    icon: Icon(
-                      FontAwesome.lock,
-                      color: Colors.grey,
-                    ),
-                    controller: _senhaControlador,
-                    validator: (text) {
-                      double forcaSenha =
-                          estimatePasswordStrength(_senhaControlador.text);
-                      if (text.length < 6) {
-                        return "A senha deve conter pelo menos 6 caracteres";
-                      }
-                      if (forcaSenha < 0.3) {
-                        return "Senha fraca, digite uma senha mais forte";
-                      }
-                      return null;
-                    },
-                    inputType: TextInputType.visiblePassword,
-                    isSenha: true),
-                SizedBox(height: 5),
-                Text("Confirmar nova Senha",
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 16,
-                      color: Colors.grey,
-                    )),
-                SizedBox(height: 5),
-                CustomFormField(
-                    hint: "**********",
-                    icon: Icon(
-                      FontAwesome.lock,
-                      color: Colors.grey,
-                    ),
-                    controller: _senhaConfirmaControlador,
-                    validator: (text) {
-                      if (text != _senhaControlador.text) {
-                        return "As senhas estão diferentes";
-                      }
-                      return null;
-                    },
-                    inputType: TextInputType.visiblePassword,
-                    isSenha: true),
-              ],
+    return ScopedModelDescendant<LoginModelo>(
+      builder: (context, child, model) {
+        if (model.dados != null)
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Editar Perfil"),
+              centerTitle: true,
+              leading: Util.leadingScaffold(context),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Container(
-              height: 45,
-              width: MediaQuery.of(context).size.width / 1.2,
-              child: RaisedButton(
-                  color: Color(0xFFf45d27),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                  onPressed: () {},
-                  child: Text(
-                    'Confirmar',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+            body: Form(
+              key: _formKey,
+              child: IgnorePointer(
+                ignoring: !_botaoHabilitado,
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(25),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Nome completo",
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 16,
+                                color: Colors.grey,
+                              )),
+                          SizedBox(height: 5),
+                          CustomFormField(
+                              hint: "Nome Completo",
+                              icon: Icon(
+                                FontAwesome.user,
+                                color: Colors.grey,
+                              ),
+                              inputType: TextInputType.emailAddress,
+                              controller: _nomeControlador,
+                              validator: (text) {
+                                return null;
+                              }),
+                          SizedBox(height: 5),
+                          Text("Telefone",
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 16,
+                                color: Colors.grey,
+                              )),
+                          SizedBox(height: 5),
+                          CustomFormField(
+                              hint: "(99)9 9999-9999",
+                              icon: Icon(
+                                FontAwesome.phone,
+                                color: Colors.grey,
+                              ),
+                              controller: _telefoneControlador,
+                              inputType: TextInputType.phone,
+                              validator: (text) {
+                                if (text.isEmpty || text.length < 11) {
+                                  return "Numero invalido";
+                                }
+                                return null;
+                              }),
+                          SizedBox(height: 5),
+                          FlatButton(
+                            onPressed: () async {
+                              await model.recuperarSenha(model.dados.email);
+                              await FlushbarHelper.createInformation(
+                                      message:
+                                          "Verifique seu email para mais sobre a alteração")
+                                  .show(context);
+                            },
+                            child: Text("Mudar senha",
+                                style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 16,
+                                  color: Theme.of(context).primaryColor,
+                                )),
+                          ),
+                        ],
+                      ),
                     ),
-                  )),
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Container(
+                        height: 45,
+                        width: MediaQuery.of(context).size.width / 1.2,
+                        child: RaisedButton(
+                          color: Color(0xFFf45d27),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() {
+                                _botaoHabilitado = false;
+                              });
+                              model.dados.nome = _nomeControlador.text;
+                              model.dados.telefone = _telefoneControlador.text;
+                              await Firestore.instance
+                                  .collection('usuarios')
+                                  .document(model.dados.id)
+                                  .setData(model.dados.toMap(), merge: true)
+                                  .then((value) async {
+                                await FlushbarHelper.createSuccess(
+                                        message:
+                                            "Informações de usuário atualizadas com sucesso")
+                                    .show(context);
+                                Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) => HomeTela()));
+                              }).catchError((e) async {
+                                await FlushbarHelper.createError(
+                                        message:
+                                            "Houve algum erro ao atualizar as informações")
+                                    .show(context);
+                                setState(() {
+                                  _botaoHabilitado = true;
+                                });
+                              });
+                            }
+                          },
+                          child: _botaoHabilitado
+                              ? Text(
+                                  'Confirmar',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              : CircularProgressIndicator(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
+          );
+        return Center();
+      },
     );
   }
 }
