0e11d898bc87daa492f5ca5d2b3e9090745a32a9
Nikollas <nikollasferreirag@gmail.com>
2020-07-17 14:25:24 -0300

    feat(agendado_tela): chama API getAgendados e recebe os json

--- lib/Telas/agendado_tela.dart
+++ lib/Telas/agendado_tela.dart
@@ -1,54 +1,68 @@
+import 'dart:convert';
+
 import 'package:agendacabelo/Controle/horario_controle.dart';
 import 'package:agendacabelo/Dados/horario.dart';
+import 'package:agendacabelo/Dados/login.dart';
+import 'package:agendacabelo/Dados/servico.dart';
 import 'package:agendacabelo/Modelos/login_modelo.dart';
 import 'package:agendacabelo/Tiles/agendado_tile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:http/http.dart' as http;
 
 class AgendadoTela extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<LoginModelo>(builder: (context, child, model) {
       if (model.dados != null) {
+        String url =
+            'https://us-central1-agendamento-cortes.cloudfunctions.net/getAgendados'
+            '?clienteId=${model.dados.id}&pago=';
         return TabBarView(
           children: <Widget>[
-            FutureBuilder<QuerySnapshot>(
-          future: HorarioControle.get()
-              .where('cliente', isEqualTo: model.dados.id)
-              .orderBy('data', descending: true)
-              .getDocuments(),
-          builder: (context, snapshot) {  
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              var tiles = snapshot.data.documents.map((doc) {
-                return AgendadoTile(Horario.fromDocument(doc));
-              }).toList();
-              return ListView(children: tiles);
-            }
-          },
-        ),
-         FutureBuilder<QuerySnapshot>(
-          future: HorarioControle.get()
-              .where('cliente', isEqualTo: model.dados.id)
-              .orderBy('data', descending: true)
-              .getDocuments(),
-          builder: (context, snapshot) {  
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              var tiles = snapshot.data.documents.map((doc) {
-                return AgendadoTile(Horario.fromDocument(doc));
-              }).toList();
-              return ListView(children: tiles);
-            }
-          },
-        ),
+            FutureBuilder<http.Response>(
+              future: http.get(url + 'false'),
+              builder: (context, response) {
+                if (!response.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  List<dynamic> dados = json.decode(response.data.body);
+                  var widgets = dados
+                      .map((dado) => AgendadoTile(
+                            horario: Horario.fromJson(dado),
+                            servico: Servico.fromHorarioJson(dado),
+                            cabeleireiro: Login.fromHorarioJson(dado),
+                            pago: false,
+                          ))
+                      .toList();
+                  return ListView(children: widgets);
+                }
+              },
+            ),
+            FutureBuilder<http.Response>(
+              future: http.get(url + 'true'),
+              builder: (context, response) {
+                if (!response.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  List<dynamic> dados = json.decode(response.data.body);
+                  var widgets = dados
+                      .map((dado) => AgendadoTile(
+                            horario: Horario.fromJson(dado),
+                            servico: Servico.fromHorarioJson(dado),
+                            cabeleireiro: Login.fromHorarioJson(dado),
+                            pago: true,
+                          ))
+                      .toList();
+                  return ListView(children: widgets);
+                }
+              },
+            ),
           ],
         );
       } else {
