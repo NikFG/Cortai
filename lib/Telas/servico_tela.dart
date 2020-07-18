c92938d3459d7ff009104d627e85281edd7f17a9
Martinho672 <martinho672@gmail.com>
2020-07-18 20:40:28 -0300

    feat(servico_tela): cria novo padrão para descrição...
    e acesso ao saiba mais

--- lib/Telas/servico_tela.dart
+++ lib/Telas/servico_tela.dart
@@ -1,12 +1,16 @@
 import 'package:agendacabelo/Controle/servico_controle.dart';
 import 'package:agendacabelo/Dados/servico.dart';
 import 'package:agendacabelo/Dados/salao.dart';
+import 'package:agendacabelo/Telas/saiba_mais.dart';
 import 'package:agendacabelo/Tiles/servico_tile.dart';
 import 'package:agendacabelo/Util/util.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:agendacabelo/Widgets/custom_appbar.dart';
 import 'package:agendacabelo/Widgets/custom_appbar_expandida.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+import 'home_tela.dart';
 
 class ServicoTela extends StatelessWidget {
   final Salao dados;
@@ -32,12 +36,12 @@ class ServicoTela extends StatelessWidget {
                   fontFamily: 'Poppins',
                   fontSize: 20.0,
                 ),
-                overflow: TextOverflow.fade,
+                overflow: TextOverflow.ellipsis,
               )),
               stretch: false,
               pinned: true,
               centerTitle: true,
-              expandedHeight: 75.0,
+              expandedHeight: 90.0,
               flexibleSpace: FlexibleSpaceBar(
                 background: CustomAppbarExpandida(
                     nomeSalao: dados.nome,
@@ -48,7 +52,109 @@ class ServicoTela extends StatelessWidget {
               )),
           SliverList(
             delegate: SliverChildListDelegate([
-              Container(color: Colors.green,height: 100,),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  
+                ),
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      child: GestureDetector(
+                        onTap: (){
+                           Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => SaibaMaisTela()));
+                        },
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                          dados.nome,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500,
+                              fontSize: 22),
+                        ),
+                        
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                        child: GestureDetector(
+                        onTap: (){
+                          print('to aqui');
+                           Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => SaibaMaisTela()));
+                        },
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.only(bottom: 8.0, right: 8.0),
+                              child: Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Container(
+                                        // child: Icon(),
+
+                                        ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Container(
+                                      child: Text(
+                                        "$distancia",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontFamily: 'Poppins',
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(bottom: 8.0, left: 8.0),
+                            child: Text(
+                              "R\$19 "
+                              "~ R\$119",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'Poppins',
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Row(
+                              children: <Widget>[
+                                SizedBox(width: 2.0),
+                                Icon(Icons.star,
+                                    color: Colors.amberAccent, size: 16.0),
+                                SizedBox(width: 5.0),
+                                Text(
+                                  '4,5',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.amber,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        Icon(FontAwesome.angle_right),
+                        ])))
+                  ],
+                ),
+              ),
               FutureBuilder<QuerySnapshot>(
                 future: ServicoControle.get()
                     .where('salao', isEqualTo: dados.id)
