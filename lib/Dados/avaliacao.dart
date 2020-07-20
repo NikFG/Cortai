1346727a9e981cb2d8bb17f38b1de8508b22f4fb
Nikollas <nikollasferreirag@gmail.com>
2020-07-20 10:51:00 -0300

    feat(avaliacao):  cria novos campos
    descrição, data e hora da avaliação

--- lib/Dados/avaliacao.dart
+++ lib/Dados/avaliacao.dart
@@ -1,18 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 class Avaliacao {
   String id;
   double avaliacao;
+  String descricao;
+  String data;
+  String hora;
   String cabeleireiro;
   String salao;
   String horario;
+
 
   Avaliacao();
 
   Avaliacao.fromDocument(DocumentSnapshot snapshot) {
     id = snapshot.documentID;
     avaliacao = snapshot.data["avaliacao"];
+    descricao =
+        snapshot.data["descricao"] == null ? "" : snapshot.data['descricao'];
+    data = snapshot.data["data"];
+    hora = snapshot.data["hora"];
     cabeleireiro = snapshot.data["cabeleireiro"];
     salao = snapshot.data['salao'];
     horario = snapshot.data['horario'];
@@ -21,9 +28,12 @@ class Avaliacao {
   Map<String, dynamic> toMap() {
     return {
       "avaliacao": avaliacao,
+      "descricao": descricao,
+      "data": data,
+      "hora": hora,
       "cabeleireiro": cabeleireiro,
       "salao": salao,
-      "horario": horario
+      "horario": horario,
     };
   }
 }
