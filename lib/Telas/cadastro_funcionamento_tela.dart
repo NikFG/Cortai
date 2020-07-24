9be92c87b317fab70c04d54faa64bf32a58b0d4e
Nikollas <nikollasferreirag@gmail.com>
2020-07-24 18:11:50 -0300

    feat(cadastro_funcionamento_tela): modifica passagem parametros
    Além disso, remove código denecessário

--- lib/Telas/cadastro_funcionamento_tela.dart
+++ lib/Telas/cadastro_funcionamento_tela.dart
@@ -1,14 +1,11 @@
 import 'package:agendacabelo/Controle/funcionamento_controle.dart';
 import 'package:agendacabelo/Dados/funcionamento.dart';
 import 'package:agendacabelo/Telas/editar_horario_funcionamento.dart';
-import 'package:agendacabelo/Telas/home_tela.dart';
 import 'package:agendacabelo/Util/util.dart';
+import 'package:agendacabelo/Widgets/custom_button.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 class CadastroFuncionamentoTela extends StatefulWidget {
   final String salao;
@@ -25,12 +22,6 @@ class _CadastroFuncionamentoTelaState extends State<CadastroFuncionamentoTela> {
   var _formKey = GlobalKey<FormState>();
   TimeOfDay selectedTime = TimeOfDay.now();
   TimeOfDay selectedTime2 = TimeOfDay.now();
-  var _aberturaController = TextEditingController();
-  var _fechamentoController = TextEditingController();
-  var _intervaloController = MaskedTextController(mask: '00');
-  bool _switchMarcado = true;
-  bool _botaoHabilitado = true;
-  List _diasSemana = [false, false, false, false, false, false, false];
 
   @override
   Widget build(BuildContext context) {
@@ -66,216 +57,43 @@ class _CadastroFuncionamentoTelaState extends State<CadastroFuncionamentoTela> {
                     Funcionamento dados = Funcionamento.fromDocument(doc);
                     return Row(
                       children: <Widget>[
-                        Text(
-                            "${dados.diaSemana}:  ",
+                        Text("${dados.diaSemana}:  ",
                             style: TextStyle(
                                 fontSize: 22, fontWeight: FontWeight.w700)),
-                                SizedBox(
-                                  height: 50,
-                                ),
+                        SizedBox(
+                          height: 50,
+                        ),
                         Text(
                             "${dados.horarioAbertura} - ${dados.horarioFechamento}",
                             style: TextStyle(
-                                fontSize: 18,))
+                              fontSize: 18,
+                            ))
                       ],
                     );
                   }).toList();
                   return Column(
-                    
                     children: listaRows,
                   );
                 }
               },
             ),
-
-            /*
-            GestureDetector(
-              onTap: () => _selectTime(context, _aberturaController),
-              child: AbsorbPointer(
-                child: TextFormField(
-                  controller: _aberturaController,
-                  decoration: InputDecoration(
-                    hintText: "Horário de abertura",
-                    prefixIcon: Icon(Icons.access_time),
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () => _selectTime(context, _fechamentoController),
-              child: AbsorbPointer(
-                child: TextFormField(
-                  autofocus: false,
-                  controller: _fechamentoController,
-                  decoration: InputDecoration(
-                    hintText: "Horário de fechamento",
-                    prefixIcon: Icon(Icons.access_time),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: _intervaloController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                hintText: "Intervalo entre horários",
-                prefixIcon: Icon(Icons.settings_backup_restore),
-              ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return "Digite um número";
-                }
-                return null;
-              },
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              children: <Widget>[
-                Switch(
-                  onChanged: (value) {
-                    setState(() {
-                      _switchMarcado = value;
-                    });
-                  },
-                  value: _switchMarcado,
-                ),
-                Text(_switchMarcado ? "Acrescentar" : "Substituir"),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text('DOM'),
-                    SizedBox(
-                      width: 42,
-                      height: 49,
-                      child: Checkbox(
-                        onChanged: (bool value) {
-                          setState(() {
-                            _diasSemana[0] = value;
-                          });
-                        },
-                        value: _diasSemana[0],
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text('SEG'),
-                    Checkbox(
-                      onChanged: (bool value) {
-                        setState(() {
-                          _diasSemana[1] = value;
-                        });
-                      },
-                      value: _diasSemana[1],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text('TER'),
-                    Checkbox(
-                      onChanged: (bool value) {
-                        setState(() {
-                          _diasSemana[2] = value;
-                        });
-                      },
-                      value: _diasSemana[2],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text('QUA'),
-                    Checkbox(
-                      onChanged: (bool value) {
-                        setState(() {
-                          _diasSemana[3] = value;
-                        });
-                      },
-                      value: _diasSemana[3],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text('QUI'),
-                    Checkbox(
-                      onChanged: (bool value) {
-                        setState(() {
-                          _diasSemana[4] = value;
-                        });
-                      },
-                      value: _diasSemana[4],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text('SEX'),
-                    Checkbox(
-                      onChanged: (bool value) {
-                        setState(() {
-                          _diasSemana[5] = value;
-                        });
-                      },
-                      value: _diasSemana[5],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text('SAB'),
-                    Checkbox(
-                      onChanged: (bool value) {
-                        setState(() {
-                          _diasSemana[6] = value;
-                        });
-                      },
-                      value: _diasSemana[6],
-                    ),
-                  ],
-                ),
-              ],
-            ),*/
             Padding(
-              padding:EdgeInsets.only(top: 100),
-              child:
-            SizedBox(
-              height: 46,
-              width: MediaQuery.of(context).size.width / 1.1,
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => EditarFuncionamentoTela()));
-                },
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.red)),
-                child: _botaoHabilitado
-                    ? Text(
-                        "Editar",
-                        style: TextStyle(fontSize: 18),
-                      )
-                    : CircularProgressIndicator(),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
+              padding: EdgeInsets.only(top: 30),
+              child: SizedBox(
+                height: 46,
+                width: MediaQuery.of(context).size.width / 1.1,
+                child: CustomButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                EditarFuncionamentoTela(widget.salao)));
+                  },
+                  textoBotao: "Editar",
+                  botaoHabilitado: true,
+                ),
               ),
-            ),
-
             ),
           ],
         ),
@@ -283,34 +101,4 @@ class _CadastroFuncionamentoTelaState extends State<CadastroFuncionamentoTela> {
     );
   }
 
-  Future<Null> _selectTime(
-      BuildContext context, TextEditingController timeController) async {
-    final TimeOfDay picked =
-        await showTimePicker(context: context, initialTime: TimeOfDay.now());
-    if (picked != null)
-      setState(() {
-        timeController.value = TextEditingValue(text: picked.format(context));
-      });
-  }
-
-  String _diaSemanaIndex(int index) {
-    switch (index) {
-      case 0:
-        return 'DOM';
-      case 1:
-        return 'SEG';
-      case 2:
-        return 'TER';
-      case 3:
-        return 'QUA';
-      case 4:
-        return 'QUI';
-      case 5:
-        return 'SEX';
-      case 6:
-        return 'SAB';
-      default:
-        return '';
-    }
-  }
 }
