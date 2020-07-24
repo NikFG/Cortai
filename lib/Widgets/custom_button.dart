dc958e25c430ff5b9a73158729e6bcbee5a874f6
Nikollas <nikollasferreirag@gmail.com>
2020-07-24 17:58:02 -0300

    feat(custom_button): modifica botão padrão para receber parametros

--- lib/Widgets/custom_button.dart
+++ lib/Widgets/custom_button.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
+  final String textoBotao;
+  final bool botaoHabilitado;
+  final VoidCallback onPressed;
+
+  CustomButton(
+      {@required this.textoBotao,
+      @required this.botaoHabilitado,
+      @required this.onPressed});
+
   @override
   Widget build(BuildContext context) {
-    bool _botaoHabilitado = true;
-    String _botaoName = "Botao";
-    return  SizedBox(
-              height: 46,
-              width: MediaQuery.of(context).size.width / 1.1,
-              child: RaisedButton(
-                onPressed: (){},
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.red)),
-                child: _botaoHabilitado
-                    ? Text(
-                        _botaoName,
-                        style: TextStyle(fontSize: 18),
-                      )
-                    : CircularProgressIndicator(),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-              ),
-            );
+    return SizedBox(
+      height: 46,
+      width: MediaQuery.of(context).size.width / 1.1,
+      child: RaisedButton(
+        onPressed: onPressed,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: BorderSide(color: Colors.red)),
+        child: botaoHabilitado
+            ? Text(
+                this.textoBotao,
+                style: TextStyle(fontSize: 18),
+              )
+            : CircularProgressIndicator(),
+        textColor: Colors.white,
+        color: Theme.of(context).primaryColor,
+      ),
+    );
   }
 }
