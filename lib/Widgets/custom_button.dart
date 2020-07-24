c196786c6b030728937ed518f4f8ff8b0d1d6847
Martinho672 <martinho672@gmail.com>
2020-07-24 16:51:54 -0300

    feat(custom_button):Cria botao padrao

--- lib/Widgets/custom_button.dart
+++ lib/Widgets/custom_button.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    bool _botaoHabilitado = true;
+    String _botaoName = "Botao";
+    return  SizedBox(
+              height: 46,
+              width: MediaQuery.of(context).size.width / 1.1,
+              child: RaisedButton(
+                onPressed: (){},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.red)),
+                child: _botaoHabilitado
+                    ? Text(
+                        _botaoName,
+                        style: TextStyle(fontSize: 18),
+                      )
+                    : CircularProgressIndicator(),
+                textColor: Colors.white,
+                color: Theme.of(context).primaryColor,
+              ),
+            );
   }
 }
