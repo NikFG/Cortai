57699271167ecc63c86bfb8f99940bc18b29efa8
Nikollas <nikollasferreirag@gmail.com>
2020-08-06 15:23:00 -0300

    feat: modifica passagem de parametros
    Agora é possível verificar se é dono de salão e o id do usuário

--- lib/Tiles/gerencia_servico_tile.dart
+++ lib/Tiles/gerencia_servico_tile.dart
@@ -5,18 +5,41 @@ import 'package:flutter/material.dart';
 
 class GerenciaServicoTile extends StatelessWidget {
   final Servico dados;
+  final bool isDonoSalao;
+  final String id;
 
-  GerenciaServicoTile(this.dados);
+  GerenciaServicoTile(
+      {@required this.dados, @required this.isDonoSalao, @required this.id});
 
   @override
   Widget build(BuildContext context) {
     return CustomListTile(
-      color: dados.ativo?Theme.of(context).cardColor:Colors.grey[300],
-      onTap: () => Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => CriarServicoTela(
-                dados: dados,
-                titulo: "Editar Serviço",
-              ))),
+      color: dados.ativo ? Theme.of(context).cardColor : Colors.grey[300],
+      onTap: () {
+        if (this.isDonoSalao || dados.cabeleireiros.contains(id))
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => CriarServicoTela(
+                    dados: dados,
+                    titulo: "Editar Serviço",
+                  )));
+        else {
+          showDialog(
+              context: context,
+              barrierDismissible: true,
+              builder: (context) => AlertDialog(
+                    content: Text(
+                        "Peça permissão ao gerente para poder acessar este corte e editá-lo"),
+                    actions: <Widget>[
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text("Ok"),
+                      ),
+                    ],
+                  ));
+        }
+      },
       leading: dados.imagemUrl != null
           ? CircleAvatar(
               radius: 30,
