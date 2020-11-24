2292d6e53bf6ad0cb9017ea6abd380c939080753
Nikollas <nikollasferreirag@gmail.com>
2020-11-24 18:32:37 -0300

    feat: modelo funcionamento preparado para api

--- lib/Dados/funcionamento.dart
+++ lib/Dados/funcionamento.dart
@@ -1,25 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Funcionamento {
+  int id;
   String diaSemana;
   String horarioAbertura;
   String horarioFechamento;
   int intervalo;
+  int salao_id;
 
   Funcionamento();
 
-/*  Funcionamento.fromDocument(DocumentSnapshot snapshot) {
-    diaSemana = snapshot.documentID;
-    horarioAbertura = snapshot.data['horarioAbertura'];
-    horarioFechamento = snapshot.data['horarioFechamento'];
-    intervalo = snapshot.data['intervalo'];
-  }*/
-
   Map<String, dynamic> toMap() {
     return {
-      "horarioAbertura": horarioAbertura,
-      "horarioFechamento": horarioFechamento,
+      "id": id,
+      "dia_semana": diaSemana,
+      "horario_abertura": horarioAbertura,
+      "horario_fechamento": horarioFechamento,
       "intervalo": intervalo,
+      "salao_id": salao_id
     };
+  }
+
+  Funcionamento.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    diaSemana = json['dia_semana'];
+    horarioAbertura = json['horario_abertura'];
+    horarioFechamento = json['horario_fechamento'];
+    intervalo = json['intervalo'];
+    salao_id = json['salao_id'];
   }
 }
