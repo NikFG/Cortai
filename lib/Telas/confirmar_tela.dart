827d86a29bbd5b8322299e14e44c731a27c2e35f
Nikollas <nikollasferreirag@gmail.com>
2020-12-03 12:04:23 -0300

    feat: adapta tela para o mobx

--- lib/Telas/confirmar_tela.dart
+++ lib/Telas/confirmar_tela.dart
@@ -1,29 +1,133 @@
-import 'dart:convert';
-
 import 'package:cortai/Controle/horario_controle.dart';
 import 'package:cortai/Dados/horario.dart';
 import 'package:cortai/Modelos/login_modelo.dart';
+import 'package:cortai/Stores/confirmar_store.dart';
 import 'package:cortai/Tiles/confirmar_tile.dart';
-import 'package:cortai/Util/util.dart';
 import 'package:cortai/Widgets/custom_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'package:http/http.dart' as http;
 
-class ConfirmarTela extends StatefulWidget {
-  @override
-  _ConfirmarTelaState createState() => _ConfirmarTelaState();
-}
+import 'package:flutter_mobx/flutter_mobx.dart';
 
-class _ConfirmarTelaState extends State<ConfirmarTela> {
+class ConfirmarTela extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ConfirmarStore store = ConfirmarStore();
     return ScopedModelDescendant<LoginModelo>(
       builder: (context, child, model) {
-        if (model.dados != null)
+        if (model.dados != null) {
+          store.getData(HorarioControle.getCabeleireiroAux(), model.token);
           return TabBarView(
             children: <Widget>[
-              FutureBuilder<http.Response>(
+              Tab(
+                child: Observer(builder: (context) {
+                  if (store.isLoading) {
+                    return CustomShimmer(4);
+                  } else {
+                    return RefreshIndicator(
+                        displacement: MediaQuery.of(context).size.width / 2,
+                        color: Theme.of(context).primaryColor,
+                        onRefresh: () => store.getData(
+                            HorarioControle.getCabeleireiroAux(), model.token),
+                        // child: store.statusCode == 200?
+                        child: Observer(
+                          builder: (context) {
+                            var widgets = store.naoConfirmados
+                                .map<Widget>((h) => ConfirmarTile(
+                                      h,
+                                      confirmadoChanged: (int value) {
+                                        int index = store.naoConfirmados
+                                            .indexWhere((element) =>
+                                                element.id == h.id);
+                                        store.teste(index);
+                                      },
+                                    ))
+                                .toList();
+                            return ListView(
+                              physics: AlwaysScrollableScrollPhysics(),
+                              children: widgets,
+                            );
+                          },
+                        ));
+                  }
+                }),
+              ),
+              Tab(
+                child: Observer(builder: (context) {
+                  if (store.isLoading) {
+                    return CustomShimmer(4);
+                  } else {
+                    return RefreshIndicator(
+                        displacement: MediaQuery.of(context).size.width / 2,
+                        color: Theme.of(context).primaryColor,
+                        onRefresh: () => store.getData(
+                            HorarioControle.getCabeleireiroAux(), model.token),
+                        child: store.statusCode == 200
+                            ? ListView.builder(
+                                physics: AlwaysScrollableScrollPhysics(),
+                                itemCount: store.contConfirmados,
+                                itemBuilder: (context, index) {
+                                  return ConfirmarTile(
+                                      store.confirmados[index]);
+                                })
+                            : ListView(
+                                physics: AlwaysScrollableScrollPhysics(),
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                        top:
+                                            MediaQuery.of(context).size.height /
+                                                4),
+                                    child: Text(
+                                      "Sem dados a confirmar",
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  )
+                                ],
+                              ));
+                  }
+                }),
+              )
+/*
+              Tab(
+                child: Observer(builder: (context) {
+                  if (store.isLoading) {
+                    return CustomShimmer(4);
+                  } else {
+                    return RefreshIndicator(
+                      displacement: MediaQuery.of(context).size.width / 2,
+                      color: Theme.of(context).primaryColor,
+                      onRefresh: () => store.getData(
+                          HorarioControle.getCabeleireiroAux()),
+                      child: jsonTrue.statusCode == 404
+                          ? ListView(
+                              physics: AlwaysScrollableScrollPhysics(),
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      top: MediaQuery.of(context).size.height /
+                                          4),
+                                  child: Text(
+                                    jsonFalse.data[0],
+                                    textAlign: TextAlign.center,
+                                  ),
+                                )
+                              ],
+                            )
+                          : ListView.builder(
+                              physics: AlwaysScrollableScrollPhysics(),
+                              itemCount: jsonTrue.data.length,
+                              itemBuilder: (context, index) {
+                                var dado = jsonTrue.data[index];
+                                var horario = Horario.fromJson(dado);
+                                return ConfirmarTile(horario);
+                              }),
+                    );
+                  }
+                }),
+              )
+*/
+/*              FutureBuilder<http.Response>(
                   future: http.get(
                       HorarioControle.getCabeleireiro('cabeleireiro', 0),
                       headers: Util.token(model.token)),
@@ -49,8 +153,8 @@ class _ConfirmarTelaState extends State<ConfirmarTela> {
                         },
                       );
                     }
-                  }),
-              FutureBuilder<http.Response>(
+                  })*/
+/*              FutureBuilder<http.Response>(
                   future: http.get(
                       HorarioControle.getCabeleireiro('cabeleireiro', 1),
                       headers: Util.token(model.token)),
@@ -74,10 +178,10 @@ class _ConfirmarTelaState extends State<ConfirmarTela> {
                         },
                       );
                     }
-                  }),
+                  })*/
             ],
           );
-        else
+        } else
           return Center();
       },
     );
