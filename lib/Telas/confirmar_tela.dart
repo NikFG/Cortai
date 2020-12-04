5e53c4bb44b95fa8a9526605df729386f4ea8651
Nikollas <nikollasferreirag@gmail.com>
2020-12-04 19:16:17 -0300

    fix: corrige observer da tela para ser reativo

--- lib/Telas/confirmar_tela.dart
+++ lib/Telas/confirmar_tela.dart
@@ -38,8 +38,8 @@ class ConfirmarTela extends StatelessWidget {
                                       confirmadoChanged: (int value) {
                                         int index = store.naoConfirmados
                                             .indexWhere((element) =>
-                                                element.id == h.id);
-                                        store.teste(index);
+                                                element.id == value);
+                                        store.mudaLista(index);
                                       },
                                     ))
                                 .toList();
@@ -62,123 +62,22 @@ class ConfirmarTela extends StatelessWidget {
                         color: Theme.of(context).primaryColor,
                         onRefresh: () => store.getData(
                             HorarioControle.getCabeleireiroAux(), model.token),
-                        child: store.statusCode == 200
-                            ? ListView.builder(
-                                physics: AlwaysScrollableScrollPhysics(),
-                                itemCount: store.contConfirmados,
-                                itemBuilder: (context, index) {
-                                  return ConfirmarTile(
-                                      store.confirmados[index]);
-                                })
-                            : ListView(
-                                physics: AlwaysScrollableScrollPhysics(),
-                                children: <Widget>[
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                        top:
-                                            MediaQuery.of(context).size.height /
-                                                4),
-                                    child: Text(
-                                      "Sem dados a confirmar",
-                                      textAlign: TextAlign.center,
-                                    ),
-                                  )
-                                ],
-                              ));
+                        child: Observer(
+                          builder: (context) {
+                            var widgets = store.confirmados
+                                .map<Widget>((h) => ConfirmarTile(
+                                      h,
+                                    ))
+                                .toList();
+                            return ListView(
+                              physics: AlwaysScrollableScrollPhysics(),
+                              children: widgets,
+                            );
+                          },
+                        ));
                   }
                 }),
               )
-/*
-              Tab(
-                child: Observer(builder: (context) {
-                  if (store.isLoading) {
-                    return CustomShimmer(4);
-                  } else {
-                    return RefreshIndicator(
-                      displacement: MediaQuery.of(context).size.width / 2,
-                      color: Theme.of(context).primaryColor,
-                      onRefresh: () => store.getData(
-                          HorarioControle.getCabeleireiroAux()),
-                      child: jsonTrue.statusCode == 404
-                          ? ListView(
-                              physics: AlwaysScrollableScrollPhysics(),
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      top: MediaQuery.of(context).size.height /
-                                          4),
-                                  child: Text(
-                                    jsonFalse.data[0],
-                                    textAlign: TextAlign.center,
-                                  ),
-                                )
-                              ],
-                            )
-                          : ListView.builder(
-                              physics: AlwaysScrollableScrollPhysics(),
-                              itemCount: jsonTrue.data.length,
-                              itemBuilder: (context, index) {
-                                var dado = jsonTrue.data[index];
-                                var horario = Horario.fromJson(dado);
-                                return ConfirmarTile(horario);
-                              }),
-                    );
-                  }
-                }),
-              )
-*/
-/*              FutureBuilder<http.Response>(
-                  future: http.get(
-                      HorarioControle.getCabeleireiro('cabeleireiro', 0),
-                      headers: Util.token(model.token)),
-                  builder: (context, response) {
-                    if (!response.hasData) {
-                      return CustomShimmer(4);
-                    } else {
-                      if (response.data.body == '[]') {
-                        return Center(
-                          child: Text("Não há agendamentos para confirmar"),
-                        );
-                      }
-                      List<Horario> listaConfirmar =
-                          jsonDecode(response.data.body)
-                              .map<Horario>((h) => Horario.fromJson(h))
-                              .toList();
-                      return ListView.builder(
-                        itemCount: listaConfirmar.length,
-                        itemBuilder: (context, index) {
-                          Horario horario = listaConfirmar[index];
-
-                          return ConfirmarTile(horario);
-                        },
-                      );
-                    }
-                  })*/
-/*              FutureBuilder<http.Response>(
-                  future: http.get(
-                      HorarioControle.getCabeleireiro('cabeleireiro', 1),
-                      headers: Util.token(model.token)),
-                  builder: (context, response) {
-                    if (!response.hasData) {
-                      return CustomShimmer(4);
-                    } else {
-                      if (response.data.body == '[]') {
-                        return Center(
-                          child: Text("Não há agendamentos para confirmar"),
-                        );
-                      }
-                      var listaConfirmar = jsonDecode(response.data.body)
-                          .map<Horario>((h) => Horario.fromJson(h))
-                          .toList();
-                      return ListView.builder(
-                        itemCount: listaConfirmar.length,
-                        itemBuilder: (context, index) {
-                          Horario horario = listaConfirmar[index];
-                          return ConfirmarTile(horario);
-                        },
-                      );
-                    }
-                  })*/
             ],
           );
         } else
