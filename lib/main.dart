f156a8e82d7991ad7dec1ef101e2fe15c2741029
Fernando <martinho672@gmail.com>
2021-01-08 22:46:34 -0300

    feat: inicia o metodo para utilizar a biblioteca sizer

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cortai/Widgets/splash_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:sizer/sizer.dart';
 
 import 'package:scoped_model/scoped_model.dart';
 import 'Controle/shared_preferences_controle.dart';
@@ -24,26 +25,38 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScopedModel<LoginModelo>(
-      model: LoginModelo(),
-      child: MaterialApp(
-        title: "Cortaí",
-        theme: ThemeData(
-            primarySwatch: Colors.orange,
-            primaryColor: Color(0xFFf45d27),
-            accentColor: Color(0xFFf5851f),
-            fontFamily: 'Poppins'),
-        debugShowCheckedModeBanner: false,
-        home: SplashCustom(logado),
-        localizationsDelegates: [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: [
-          const Locale('pt'),
-        ],
-      ),
+    return LayoutBuilder(
+      //return LayoutBuilder necessário para o uso da biblioteca Sizer!
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          //return OrientationBuilder necessário para o uso da biblioteca Sizer!
+          builder: (context, orientation) {
+            //initialize SizerUtil() necessário para o uso da biblioteca Sizer!
+            SizerUtil().init(constraints, orientation);
+            return ScopedModel<LoginModelo>(
+              model: LoginModelo(),
+              child: MaterialApp(
+                title: "Cortaí",
+                theme: ThemeData(
+                    primarySwatch: Colors.orange,
+                    primaryColor: Color(0xFFf45d27),
+                    accentColor: Color(0xFFf5851f),
+                    fontFamily: 'Poppins'),
+                debugShowCheckedModeBanner: false,
+                home: SplashCustom(logado),
+                localizationsDelegates: [
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+                supportedLocales: [
+                  const Locale('pt'),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
