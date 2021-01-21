4a97416e9f983ccab5355da4abd717b6a476c319
Nikollas <nikollasferreirag@gmail.com>
2021-01-21 16:07:27 -0300

    feat: modifica para receber dados do servidor

--- lib/Telas/galeria_tela.dart
+++ lib/Telas/galeria_tela.dart
@@ -1,12 +1,20 @@
+import 'dart:convert';
+
+import 'package:cortai/Controle/galeria_controle.dart';
+import 'package:cortai/Dados/galeria.dart';
 import 'package:cortai/Modelos/login_modelo.dart';
+import 'package:cortai/Tiles/galeria_tile.dart';
+import 'package:cortai/Util/util.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:sizer/sizer.dart';
-
+import 'package:http/http.dart' as http;
 import 'galeria_detalhes_tela.dart';
 
 class GaleriaTela extends StatefulWidget {
-  GaleriaTela();
+  final int salaoId;
+
+  GaleriaTela(this.salaoId);
 
   @override
   _GaleriaTelaState createState() => _GaleriaTelaState();
@@ -25,48 +33,36 @@ class _GaleriaTelaState extends State<GaleriaTela> {
         return Scaffold(
             appBar: AppBar(
               title: Text('Galeria de Barber Shop'),
+              leading: Util.leadingScaffold(context),
             ),
+
             //  backgroundColor: Theme.of(context).primaryColor,
             body: Padding(
               padding: EdgeInsets.symmetric(horizontal: 2.0.w, vertical: 2.0.h),
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                itemBuilder: (context, index) {
-                  return RawMaterialButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DetalhesGaleria(
-                            imagePath: 'assets/images/mala.png',
-                            title: 'Corte Brabo',
-                            price: '15,00',
-                            details:
-                                'malaia tem que tomar cuidado com oque escreve porque se escrever muito provavelmente vai dar ruim',
-                            index: index,
-                          ),
-                        ),
-                      );
-                    },
-                    child: Hero(
-                      tag: 'logo$index',
-                      child: Container(
-                        decoration: BoxDecoration(
-                          // borderRadius: BorderRadius.circular(5),
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/mala.png'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+              child: FutureBuilder<http.Response>(
+                future: http.get(GaleriaControle.get(widget.salaoId),
+                    headers: Util.token(model.token)),
+                builder: (context, response) {
+                  if (!response.hasData) {
+                    return CircularProgressIndicator();
+                  } else {
+                    List<dynamic> dados = json.decode(response.data.body);
+                    print(dados);
+                    return GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
                       ),
-                    ),
-                  );
+                      itemCount: dados.length,
+                      itemBuilder: (context, index) {
+                        Galeria galeria = Galeria.fromJson(dados[index]);
+                        return GaleriaTile(galeria);
+                      },
+                    );
+                  }
                 },
-                itemCount: 12,
+
               ),
             ));
       },
