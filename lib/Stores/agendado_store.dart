66a7cc8e380bda24c22f2778a0061d7fa0b3ad45
Nikollas <nikollasferreirag@gmail.com>
2021-01-21 16:10:14 -0300

    feat: modifica token para required

--- lib/Stores/agendado_store.dart
+++ lib/Stores/agendado_store.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:cortai/Util/util.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mobx/mobx.dart';
 
@@ -20,7 +21,7 @@ abstract class _AgendadoStore with Store {
   int statusCode = 400;
 
   @action
-  Future<void> getData(String url, {String token}) async {
+  Future<void> getData(String url, {@required String token}) async {
     isLoading = true;
     var response = await http.get(url, headers: Util.token(token));
     statusCode = response.statusCode;
