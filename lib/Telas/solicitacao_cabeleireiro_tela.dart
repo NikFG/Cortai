a3c824fa40e6bd51b6de26553ead6c2f0c456d28
Fernando <martinho672@gmail.com>
2021-04-15 12:40:43 -0300

    feat: adiciona libs null safety e corrige chamadas

--- lib/Telas/solicitacao_cabeleireiro_tela.dart
+++ lib/Telas/solicitacao_cabeleireiro_tela.dart
@@ -3,10 +3,10 @@ import 'package:cortai/Modelos/login_modelo.dart';
 import 'package:cortai/Widgets/button_custom.dart';
 import 'package:cortai/Widgets/form_field_custom.dart';
 import 'package:email_validator/email_validator.dart';
-import 'package:flushbar/flushbar_helper.dart';
+import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'package:sizer/sizer.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 import 'index_tela.dart';
 
