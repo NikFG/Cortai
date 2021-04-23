fe4172d8889b5bdd7c1e3132e3fb9cb96ff23479
Nikollas <nikollasferreirag@gmail.com>
2021-04-23 20:32:35 -0300

    feat: melhora envio de imagens

--- lib/Controle/galeria_controle.dart
+++ lib/Controle/galeria_controle.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:cortai/Dados/galeria.dart';
 import 'package:cortai/Util/api.dart';
 import 'package:cortai/Util/util.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class GaleriaControle {
@@ -10,12 +13,18 @@ class GaleriaControle {
     return Uri.parse(_url + salaoId.toString());
   }
 
-  static Future<void> store(Galeria dados, String token,
-      {required VoidCallback onSuccess,
+  static Future<void> store(
+      {required Galeria dados,
+      required File imagem,
+      required String token,
+      required VoidCallback onSuccess,
       required void onFail(String error)}) async {
     Api api = Api();
+    Map<String, dynamic> map = dados.toJson();
+    map["imagem"] = await MultipartFile.fromFile(imagem.path,
+        filename: imagem.path.split('/').last);
     try {
-      await api.store(_url, dados.toJson(), token);
+      await api.store(_url, map, token);
       onSuccess();
     } catch (e) {
       onFail(e.toString());
